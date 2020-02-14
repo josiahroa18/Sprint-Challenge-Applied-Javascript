8d9cdb0b041209628044e732ad166f3c7ee9c9b8
Josiah Roa <josiahroa18@gmail.com>
2020-02-14 09:50:04 -0700

    Added Carousel with no functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,33 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector('.carousel-container');
+
+function Carousel(){
+  // Create Elements
+  const carousel = document.createElement('div'),
+        leftButton = document.createElement('div'),
+        rightButton = document.createElement('div');
+
+  // Nest Elements
+  carousel.appendChild(leftButton);
+  const imgNames = ['mountains.jpeg', 'computer.jpeg', 'trees.jpeg', 'turntable.jpeg'];
+  for(let i=0; i<imgNames.length; i++){
+    let img = document.createElement('img');
+    img.src = `./assets/carousel/${imgNames[i]}`;
+    carousel.appendChild(img);
+  }
+  carousel.appendChild(rightButton);
+
+  // Add Classes
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  // Carousel Functionality
+
+  return carousel;
+}
+console.log(Carousel());
+carouselContainer.appendChild(Carousel());
